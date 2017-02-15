24407f24c93f46197e784aefce5fb8e7d5f769a6
Ayush Sharma <ayushs1729@gmail.com>
2017-02-16 04:31:55 +0530

    Add files via upload

--- heap_2.py
+++ heap_2.py
@@ -1,3 +1,5 @@
+#heap module for priority-queue
+
 def parent(i):
 	return (i - 1)//2
 
@@ -7,32 +9,33 @@ def left(i):
 def right(i):
 	return (2*i + 2)
 
-def max_heapify(a, i, n):
+def max_heapify(a, i, length):
 	l = left(i)
 	r = right(i)
 	largest = i
-	if (l < n and a[largest] < a[l]):
+	if (l < length and a[largest] < a[l]):
 		largest = l
-	if (r < n and a[largest] < a[r]):
+	if (r < length and a[largest] < a[r]):
 		largest = r
 	if (largest != i):
 		a[i],a[largest] = a[largest],a[i]
-		max_heapify(a, largest, n)
+		max_heapify(a, largest, length)
 
-def build_max_heap(a):
-	length = len(a)
+def build_max_heap(a, length):
 	for i in range(parent(length - 1), -1, -1):
 		max_heapify(a, i, length)
 
 def heap_sort(a):
-	build_max_heap(a)
-	for i in range(len(a) - 1):
-		a[0],a[len(a) - 1 - i] = a[len(a) - 1 - i],a[0]
+	length = len(a)
+	build_max_heap(a, length)
+	for i in range(length - 1):
+		a[0],a[length - 1 - i] = a[length - 1 - i],a[0]
 #		{decrease size} len(a)--
 	#	max_heapify(a[:(len(a) - 1 - i)],0)
-		a_ = a[:(len(a) - 1 - i)]		
-		max_heapify(a_,0)
-		a[:(len(a) - 1 - i)] = a_
+#		a_ = a[:(len(a) - 1 - i)]		
+#		max_heapify(a_,0)
+#		a[:(len(a) - 1 - i)] = a_
+		max_heapify(a, 0, (length - 1 - i))
 '''
 a = [1,3,5,2,7,4,0]
 heap_sort(a)
--- priority_queue.py
+++ priority_queue.py
@@ -1,43 +1,53 @@
-from heap_2.py import *
+from heap_2 import *
+#import heap_2
 
 class prq():
 	def __init__(self, a):
 		self.heap = a
-	#	self.n = len(a) - 1
-		build_max_heap(self.heap)
-
-	def insert(self, x):
-		self.heap.append(x)
-		self.increase_key(self.n)
-		#self.n += 1
+		self.n = len(a)
+		build_max_heap(self.heap, self.n)
 
 	def maximum(self):
 		return self.heap[0]
 
-	def remove_maximum(self):
+	def remove_maximum(self): #may include list.pop()
 		if len(self.heap) < 1:
-			raise EXCEPTION ("Heap underflow")
+			raise Exception ("Heap underflow")
 		else:
-			self.heap[0], self.heap[self.n] = self.heap[self.n], self.heap[0]
-		#	self.n -= 1
-			max_heapify(self.heap, 0)
+			self.heap[0], self.heap[self.n - 1] = self.heap[self.n - 1], self.heap[0]
+			self.n -= 1
+			max_heapify(self.heap, 0, self.n)
+		return self.heap[self.n]
 
 	def increase_key(self, i):
-		while (i >= 0):
-			if (self.heap[i] > self.heap[parent(i)]):
-				self.heap[i], self.heap[parent(i)] = self.heap[parent(i)], self.heap[i]
+		while (i > 0 and self.heap[i] > self.heap[parent(i)]):
+			#if (self.heap[i] > self.heap[parent(i)]):
+			self.heap[i], self.heap[parent(i)] = self.heap[parent(i)], self.heap[i]
 			i = parent(i)
+
+	def insert(self, x):
+		if (self.n < len(self.heap)):
+			self.heap[self.n] = x
+		elif(self.n == len(self.heap)):
+			self.heap.append(x)
+		self.n += 1
+		self.increase_key(self.n - 1)
 
 	def print_heap(self):
 		print(self.heap[:self.n])
-
+'''
 a = prq([1,2,3,4,5,6,7,8,9])
 
 a.insert(0)
+a.print_heap()
 
 print(a.maximum())
 
 a.insert(11)
+a.print_heap()
+
+build_max_heap(a.heap, a.n)
+a.print_heap()
 
 print(a.maximum())
 a.remove_maximum()
